6c1ec5172898ff50a6459182619927e41c6cff09
vicGeo <vicsanig@alumni.upv.es>
2021-04-27 16:16:41 +0200

    added method pause() and stop() to timer

--- src/App.vue
+++ src/App.vue
@@ -22,9 +22,16 @@
         </div>
       </div>
       <div class="ml-4 mt-4 flex-shrink-0 flex">
-        <basic-button-entry-in/>
-        <basic-button-entry-out/>
-
+        <div v-if="employeeWorking">
+          <button v-on:click="start(); toogleInOut();" class="relative inline-flex items-center mx-1 px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-100">
+          Entry In
+          </button>
+        </div>
+        <div v-else>
+          <button v-on:click="pause(); toogleInOut();" class="relative inline-flex items-center mx-1 px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-500 hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-100">
+          Entry Out
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -37,8 +44,7 @@ import axios from 'axios';
 const token = '9a4c685e6fca0347ca0775119e9c47b6ba00e49096ba99ffcf6a41fe06e6f001';
 
 import Timer from './components/Timer.vue';
-import BasicButtonEntryIn from './components/BasicButtonEntryIn.vue';
-import BasicButtonEntryOut from './components/BasicButtonEntryOut.vue';
+
 
 
 
@@ -46,8 +52,6 @@ export default {
   name: 'App',
   components: {
     Timer,
-    BasicButtonEntryIn,
-    BasicButtonEntryOut,
 
   },
   data() {
@@ -103,6 +107,55 @@ export default {
       console.log(response.data);
 
       }
+    },
+
+    async pause() {
+      window.clearInterval(this.ticker);
+      this.timerState = 'paused';
+
+      // POST workEntryOutasync
+      let workOut = {
+        "employeeId": "cf4e03eb-6945-4fa5-ada7-f902492303c4",
+          "workEntryOut": {
+            "coordinates": {
+              "latitude": null,
+              "longitude": null
+              }
+          }
+      }
+      let response = await axios.post("http://localhost:8080/schedule/v1/work-entries/clock-out", workOut, {
+        headers: {
+          Authorization: 'Bearer ' + token
+        }
+      })
+      console.log(response.data);
+    },
+
+    async stop() {
+
+      if (this.timerState == 'running') {
+        window.clearInterval(this.ticker);
+        this.currentTimer = 0;
+        this.formattedTime = "00:00:00"
+        this.timerState = "stopped"
+      }
+
+      // POST workEntryOutasync
+      let workOut = {
+        "employeeId": "cf4e03eb-6945-4fa5-ada7-f902492303c4",
+          "workEntryOut": {
+            "coordinates": {
+              "latitude": null,
+              "longitude": null
+              }
+          }
+      }
+      let response = await axios.post("http://localhost:8080/schedule/v1/work-entries/clock-out", workOut, {
+        headers: {
+          Authorization: 'Bearer ' + token
+        }
+      })
+      console.log(response.data);
     },
 
     // Logic timer HH:MM:SS
