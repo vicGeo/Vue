bd2e6fa1881d9c19f3d1cb894f750a5a10f11353
vicGeo <vicsanig@alumni.upv.es>
2021-04-27 13:24:51 +0200

    solved CORS problem with proxy in vue.config.js

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,13 @@
         </div>
       </div>
       <div class="ml-4 mt-4 flex-shrink-0 flex">
-        <button type="button" class="relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <button type="button" class="relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-100">
           <!-- <PhoneIcon class="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" /> -->
           <span>
             Phone
           </span>
         </button>
-        <button type="button" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <button type="button" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-100">
           <!-- <MailIcon class="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" /> -->
           <span>
             Email
