cef73777af2803dece0555324e989cd4f3a5a801
vicGeo <vicsanig@alumni.upv.es>
2021-04-27 13:11:00 +0200

    styled card employee

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
-    <div class="md:flex md:items-center md:justify-between">
-    <div class="flex-1 min-w-0">
-      <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-        <img alt="Vue logo" src="./assets/logo.png">
-      </h2>
-    </div>
-    <div class="mt-4 flex md:mt-0 md:ml-4">
-      <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-        Edit
-      </button>
-      <button type="button" class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-        Publish
-      </button>
+  <header>
+  <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
+    <div class="-ml-4 -mt-4 flex justify-between items-center flex-wrap sm:flex-nowrap">
+      <div class="ml-4 mt-4">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+          </div>
+          <div class="ml-4">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+              Tom Cook
+            </h3>
+            <p class="text-sm text-gray-500">
+              <a href="#">
+                @tom_cook
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="ml-4 mt-4 flex-shrink-0 flex">
+        <button type="button" class="relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <!-- <PhoneIcon class="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" /> -->
+          <span>
+            Phone
+          </span>
+        </button>
+        <button type="button" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <!-- <MailIcon class="-ml-1 mr-2 h-5 w-5 text-gray-400" aria-hidden="true" /> -->
+          <span>
+            Email
+          </span>
+        </button>
+      </div>
     </div>
   </div>
+  </header>
 </template>
 
 
-<script>
-
-export default {
-  name: 'App',
-  components: {
-
-  }
-}
-</script>
-
 <style>
+
+* {
+  box-sizing: border-box;
+}
+
+#app body {
+  margin: 0;
+}
+
+#app header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #80CBC4;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
 
 </style>
